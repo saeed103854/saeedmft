44187efa481483337c4143435680c4a9233a5d5c
Saeed <saeed.m20@gmail.com>
2024-05-06 20:24:10 +0330

    index

---
+++ models.py
@@ -0,0 +1,16 @@
+from peewee import *
+
+db = SqliteDatabase('school.db')
+
+
+class Student(Model):
+    name = CharField()
+    age = IntegerField()
+
+    class Meta:
+        database = db
+        db_table = 'Student'
+
+
+Student.create_table()
+
